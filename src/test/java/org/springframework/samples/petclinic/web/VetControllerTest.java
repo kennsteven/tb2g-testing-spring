eb2bb49607e2bf4b20cb0a74345729bb2f952a0a
kenny_alvarado <akennsteven@gmail.com>
2023-01-23 23:09:58 +0100

    adding tst with mock Mvc

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -36,11 +41,23 @@ class VetControllerTest {
 
     List<Vet> vetList = new ArrayList<>();
 
+    MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
 
         vetList.add(new Vet());
         given(clinicService.findVets()).willReturn(vetList);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vetController).build();
+    }
+
+    @Test
+    void testControllerShowVetList() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
     }
 
     @Test
